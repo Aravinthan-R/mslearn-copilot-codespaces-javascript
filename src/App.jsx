bfd3d051f9ac70d10b612f9de944dfc7599e9cc0
Aravinthan-R <105979752+Aravinthan-R@users.noreply.github.com>
2026-03-19 11:22:20 +0000

    Update siteProps with personal information and enhance social icon styles with animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Aravinthan Ramasamy",
+  title: "QA Automation Test Engineer",
+  email: "aravinthan.ramasamy@aspiresys.com",
+  gitHub: "github",
+  instagram: "instagram",
+  linkedIn: "aravinthan ramasamy",
+  medium: "medium",
+  twitter: "twitter",
+  youTube: "youTube",
 };
 
 const primaryColor = "#4E567E";
